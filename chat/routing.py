20e0aa09219fdd7badec8b992b178ac8c2320152
JoelJoby <joeljoby369@gmail.com>
2026-02-19 01:38:16 +0530

    configured Redis and Channels

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
+from . import consumers
 
 websocket_urlpatterns = [
-    # Path to consumers will go here
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
--- core/settings.py
+++ core/settings.py
@@ -77,7 +77,10 @@ ASGI_APPLICATION = 'core.asgi.application'
 # Channel Layer
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
