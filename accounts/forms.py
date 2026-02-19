0034379a3defe472fade26eda965b2576e73fe8d
JoelJoby <joeljoby369@gmail.com>
2026-02-19 20:25:35 +0530

    Login and sign in set

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,15 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        return email.lower()
 
 class UserLoginForm(AuthenticationForm):
     username = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        return username.lower()
