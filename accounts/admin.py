079a3b10b6e647d30bb474c5ec4fa193ba1c5aa6
JoelJoby <joeljoby369@gmail.com>
2026-02-19 21:00:25 +0530

    authentication done

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     list_display = ('email', 'username', 'is_online', 'last_seen', 'is_staff')
     list_filter = ('is_online', 'is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('email', 'username')
@@ -10,10 +13,17 @@ class CustomUserAdmin(UserAdmin):
     
     # Define fieldsets to include custom fields
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('email', 'is_online', 'last_seen')}),
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'is_online', 'last_seen')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password'),
+        }),
     )
 
 admin.site.register(CustomUser, CustomUserAdmin)
